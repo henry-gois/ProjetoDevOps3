13a3918113190e2dd1cb92345d2329c7df36fc7d
henry-gois <night.xxxx12@gmail.com>
2023-04-25 16:46:31 -0300

    Subindo alterações

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,9 @@ public class Main {
     public static void main(String[] args) {
         Pessoa pessoa1 = new Pessoa("Eduardo", "123456789", "11941897078", "01/01/2001", "25/12/2023", "Tarde");
         Pessoa pessoa2 = new Pessoa("Eduardo Do rolimã", "123456789-0", "11941897070", "01/01/2010", "25/12/2020", "Manhã");
+
+        System.out.println(pessoa1.toString());
+        System.out.println(pessoa2.toString());
     }
+
 }
